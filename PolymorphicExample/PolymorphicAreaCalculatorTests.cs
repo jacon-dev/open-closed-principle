60cf7b17265742d9e71607e9710919c74f49d559
James Conlin <jacon.dev@outlook.com>
2022-01-12 21:17:02 +0000

    Making some changes to naming and using object initialisation

--- PolymorphicExample/PolymorphicAreaCalculatorTests.cs
+++ PolymorphicExample/PolymorphicAreaCalculatorTests.cs
@@ -5,31 +5,35 @@ namespace OpenClose.PolymorphicExample
     [TestFixture]
     public class PolymorphicAreaCalculatorTests
     {
-        private PolymorphicAreaCalculator sut;
+        private PolymorphicAreaCalculator _sut;
         
         [SetUp]
         public void Setup()
         {
-            sut = new PolymorphicAreaCalculator();
+            _sut = new PolymorphicAreaCalculator();
         }
         
         [Test]
         public void TestThatRectangleCalculatesAsExpected()
         {
-            var rectangle = new Rectangle();
-            rectangle.Height = 10;
-            rectangle.Width = 15;
-            
-            Assert.AreEqual(150, sut.CalculateShapeArea(new object[]{ rectangle }));
+            var rectangle = new Rectangle
+            {
+                Height = 10,
+                Width = 15
+            };
+
+            Assert.AreEqual(150, _sut.CalculateShapeArea(new object[]{ rectangle }));
         }
         
         [Test]
         public void TestThatCircleCalculatesAsExpected()
         {
-            var circle = new Circle();
-            circle.Radius = 10;
-            
-            Assert.AreEqual(314.16, sut.CalculateShapeArea(new object[]{ circle }));
+            var circle = new Circle
+            {
+                Radius = 10
+            };
+
+            Assert.AreEqual(314.16, _sut.CalculateShapeArea(new object[]{ circle }));
         }
     }
 }
